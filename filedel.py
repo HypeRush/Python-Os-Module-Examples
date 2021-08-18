130dbd8ec99b01cefe6e4843d00fa743ee0ed9bd
HypeRush <67840231+HypeRush@users.noreply.github.com>
2021-08-18 13:49:17 +0300

    Update filedel.py

--- filedel.py
+++ filedel.py
@@ -1,29 +1,43 @@
 import os
 
+
 print (os.name)
 print("şuan bulunduğun dizin:",os.getcwd())
 path = input("Taranacak yerin pathi:",)
-if path == (""):
-    print (path , "Hiç bir yer bulunamadı")  
-else:
-    dir_list = os.listdir(path) 
-    print("Aranan yerdeki dosyalar ve klasörler'", path, "' :") 
-    print(dir_list) 
-    goatten = input("Silmek istediğiniz dosyanın adını yazın(silmeyecekseniz boş bırakın)",)
-    if goatten == (""):
+
+
+
+
+def main(path):
+    if path == (""):
+        path = os.getcwd()
+        dir_list = os.listdir(path)
+        print("Aranan yerdeki dosyalar ve klasörler'", path, "' :") 
+        print(dir_list) 
+        goatten = input("Silmek istediğiniz dosyanın adını yazın(silmeyecekseniz boş bırakın)",)
+        if goatten == (""):
           print (goatten , "Hiç bir şey silinmedi")    
-    else: 
+        else: 
+          print(goatten, "adlı dosya siliniyor")
+          filewillbedeleted = os.path.join(path, goatten) 
+          os.remove(filewillbedeleted)
+          print(goatten, "adlı dosya başarıyla silindi")
+
+    else:
+        dir_list = os.listdir(path) 
+        print("Aranan yerdeki dosyalar ve klasörler'", path, "' :") 
+        print(dir_list) 
+        goatten = input("Silmek istediğiniz dosyanın adını yazın(silmeyecekseniz boş bırakın)",)
+        if goatten == (""):
+          print (goatten , "Hiç bir şey silinmedi")    
+        else: 
           print(goatten, "adlı dosya siliniyor")
           filewillbedeleted = os.path.join(path, goatten) 
           os.remove(filewillbedeleted)
           print(goatten, "adlı dosya başarıyla silindi")
 
 
-
-
-
-  
-
-
+if __name__ == "__main__":
+      main(path)
 
 
