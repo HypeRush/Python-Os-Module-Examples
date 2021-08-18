5a2365165183cd38b8b1f8dd401ac0a0075d2bd1
HypeRush <67840231+HypeRush@users.noreply.github.com>
2021-08-18 13:31:30 +0300

    Update metafile.py

--- metafile.py
+++ metafile.py
@@ -1,19 +1,44 @@
 import os
-print (os.name)
+if os.name == "nt":
+      operatingsys = "windows"
+elif os.name == "posix" or os.name == "Linux" or os.name == "Linux2":
+      operatingsys == "posix"
+else:
+      operatingsys == "Cannot find your operating system via os module" 
+
+
+print(operatingsys)
 print("şuan bulunduğun dizin:",os.getcwd())
 path = input("Taranacak yerin pathi:",)
-if path == (""):
-    print (path , "Hiç bir yer bulunamadı")  
-else:
-    dir_list = os.listdir(path) 
-    print("Aranan yerdeki dosyalar ve klasörler'", path, "' :") 
-    print(dir_list) 
-    goatten = input("Sorgulamak istediğiniz dosyanın adını yazın(sorgulamayacaksanız boş bırakın)",)
-    if goatten == (""):
+
+
+def main(path):
+    if path == (""):
+      path = os.getcwd()
+      dir_list = os.listdir(path) 
+      print("Aranan yerdeki dosyalar ve klasörler'", path, "' :") 
+      print(dir_list) 
+      goatten = input("Sorgulamak istediğiniz dosyanın adını yazın(sorgulamayacaksanız boş bırakın)",)
+      if goatten == (""):
           print (goatten , "Hiç bir şey sorgulanmadı")    
-    else: 
+      else: 
+          print(goatten, "adlı dosya sorgulanıyor...")
+          print(os.stat(goatten))
+          print(goatten, "adlı dosya başarıyla sorgulandı")
+    else:
+        dir_list = os.listdir(path) 
+        print("Aranan yerdeki dosyalar ve klasörler'", path, "' :") 
+        print(dir_list) 
+        goatten = input("Sorgulamak istediğiniz dosyanın adını yazın(sorgulamayacaksanız boş bırakın)",)
+        if goatten == (""):
+          print (goatten , "Hiç bir şey sorgulanmadı")    
+        else: 
           print(goatten, "adlı dosya sorgulanıyor...")
           print(os.stat(goatten))
           print(goatten, "adlı dosya başarıyla sorgulandı")
 
+
+
+if __name__ == "__main__":
+      main(path)
 
